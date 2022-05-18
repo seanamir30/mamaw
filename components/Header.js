d4a082670987fb6f68ffa5a6c8bff482155808ab
sean.cafe <sean.cafe@oqulo.com>
2022-05-18 22:38:40 +0800

    Added Signout

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import AuthModal from '../components/modals/AuthModal';
+import { useAuth } from '../context/AuthUserContext';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { authUser,signOut } = useAuth();
+
+    const loggedInButtons = () => {
+        return(
+            <>
+                <span>{authUser.email}</span>
+                <button onClick={signOut}>Signout</button>
+            </>
+        )
+    }
+    return (
+        <>
+        {isModalOpen && <AuthModal setIsModalOpen={setIsModalOpen}/>}
+        {authUser ? loggedInButtons() : <button onClick={()=>{setIsModalOpen(!isModalOpen)}}>login</button>}
+        </>
+    )
 }
 
 export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,13 @@
 import { useState } from 'react';
 import Footer from '../components/Footer';
-import Header from '../components/Hero';
+import Header from '../components/Header';
 import Hero from '../components/Hero';
-import AuthModal from '../components/modals/AuthModal';
-import { useAuth } from '../context/AuthUserContext';
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { authUser } = useAuth();
-  console.log(authUser)
 
   return (
     <div>
       <Header/>
-      {isModalOpen && <AuthModal setIsModalOpen={setIsModalOpen}/>}
-      <button onClick={()=>{setIsModalOpen(!isModalOpen)}}>login</button>
       <Hero/>
       <Footer/>
     </div>
